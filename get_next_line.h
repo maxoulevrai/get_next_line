df85773e75496e02378dc2de6dd0b1bbc322249a
root <root@LAPTOP-6I81QJ0S.>
2025-02-27 19:53:58 +0100

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 19:03:31 by root              #+#    #+#             */
+/*   Updated: 2025/02/27 19:50:45 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+# define BUFFER_SIZE 100
+
+char	*get_next_line(int fd);
+size_t	gnl_len(void *buf);
+
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 19:07:45 by root              #+#    #+#             */
+/*   Updated: 2025/02/27 19:40:02 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	gnl_len(void *buf)
+{
+	size_t	i;
+
+	i = 0;
+	while (((char *)buf)[i] != '\n')
+		i++;
+	return (i);
+}
+
+char	*strndup(char *src, size_t siz)
+{
+	char	*dst;
+	size_t	i;
+
+	i = 0;
+	dst = NULL;
+	if (!src)
+		return (NULL);
+	dst = malloc(sizeof(char) * siz + 1);
+	if (!dst)
+		return (NULL);
+	while (src[i] && i < siz - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
