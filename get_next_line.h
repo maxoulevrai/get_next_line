c5528aab175a1f4ea498a9124d43eaacc7d2c9f7
Maxime Leca <maleca@f1r4s4.clusters.42paris.fr>
2025-08-01 17:39:47 +0200

    reorga

--- get_next_line.h
+++ get_next_line.h
@@ -3,30 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: maleca <maleca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/27 19:03:31 by root              #+#    #+#             */
-/*   Updated: 2025/03/09 20:32:47 by root             ###   ########.fr       */
+/*   Created: 2025/05/03 19:12:54 by maleca            #+#    #+#             */
+/*   Updated: 2025/05/12 20:52:31 by maleca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
-
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 1000
 # endif
 
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# include <unistd.h>
+# include <stdio.h>
+
 char	*get_next_line(int fd);
-size_t	ft_strlen_gnl(char *str);
-char	*ft_strchr_gnl(char *s, int c);
-char	*ft_strjoin_gnl(char *stash, char *buf);
-char	*extract_line(char *stash);
+char	*ft_strchr_gnl(char *str, char c);
 void	stash_cleanup(char **stash);
-void	*ft_calloc(size_t nmemb, size_t size);
+char	*read_and_store(int fd, char *stash, int *flag);
+size_t	gnl_len(char *str);
+char	*ft_strjoin_gnl(char *stash, char *buff);
 
 #endif
