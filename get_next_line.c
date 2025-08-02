d05e75a4a0098c246deda06a5eb994cdc9b3dc0e
root <root@DESKTOP-AQPOE75.localdomain>
2025-08-02 02:39:47 +0200

    OK

---
+++ get_next_line.c
@@ -0,0 +1,154 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 19:03:08 by maleca            #+#    #+#             */
+/*   Updated: 2025/08/02 02:37:51 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*extract_line(char *stash)
+{
+	size_t	i;
+	char	*line;
+
+	line = NULL;
+	if (!stash || !stash[0])
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+void	stash_cleanup(char **stash)
+{
+	size_t	i;
+	size_t	j;
+	char	*new_stash;
+
+	if (!*stash || !ft_strchr_gnl(*stash, '\n'))
+	{
+		free(*stash);
+		*stash = NULL;
+		return ;
+	}
+	i = 0;
+	while ((*stash)[i] && (*stash)[i] != '\n')
+		i++;
+	if ((*stash)[i] == '\n')
+		i++;
+	new_stash = malloc(sizeof(char) * (gnl_len(*stash) - i + 1));
+	if (!new_stash)
+		return ;
+	j = 0;
+	while ((*stash)[i])
+		new_stash[j++] = (*stash)[i++];
+	new_stash[j] = '\0';
+	free(*stash);
+	*stash = new_stash;
+}
+
+char	*read_and_store(int fd, char *stash, int *flag)
+{
+	char		*buff;
+	ssize_t		read_bytes;
+
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	read_bytes = 1;
+	while (read_bytes > 0 && !ft_strchr_gnl(stash, '\n'))
+	{
+		read_bytes = read(fd, buff, BUFFER_SIZE);
+		if (read_bytes == -1)
+			return (free(buff), NULL);
+		buff[read_bytes] = '\0';
+		stash = ft_strjoin_gnl(stash, buff);
+		if (!stash)
+			return (free(buff), NULL);
+	}
+	if (read_bytes == 0)
+		*flag = 1;
+	free(buff);
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*next_line;
+	int			flag;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+	{
+		if (stash)
+		{
+			free(stash);
+			stash = NULL;
+		}
+		return (NULL);
+	}
+	flag = 0;
+	stash = read_and_store(fd, stash, &flag);
+	if (!stash)
+		return (free(stash), NULL);
+	next_line = extract_line(stash);
+	stash_cleanup(&stash);
+	if (flag == 1)
+		free(stash);
+	return (next_line);
+}
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	line = NULL;
+// 	fd = open("", O_RDWR);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	close(fd);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	free(line);
+// 	close(fd);
+// 	return (0);
+// }
---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 19:12:54 by maleca            #+#    #+#             */
+/*   Updated: 2025/08/02 02:38:44 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr_gnl(char *str, char c);
+void	stash_cleanup(char **stash);
+char	*read_and_store(int fd, char *stash, int *flag);
+size_t	gnl_len(char *str);
+char	*ft_strjoin_gnl(char *stash, char *buff);
+
+#endif
